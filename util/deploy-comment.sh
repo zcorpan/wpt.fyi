2408952bc1c127f35c3000f8fa37db5d3c4358d9
Luke Bjerring <lukebjerring@google.com>
2018-04-12 17:30:58 -0400

    Actually use output + Improve error messages in deploy

--- util/deploy-comment.sh
+++ util/deploy-comment.sh
@@ -6,27 +6,27 @@
 set -e
 
 REPO_DIR="$(dirname "$0")/.."
-source "${TRAVIS_REPO_DIR}/util/logging.sh"
+source "${REPO_DIR}/util/logging.sh"
 
 DEPLOYED_URL=$1
 if [[ -z ${DEPLOYED_URL} ]]; then fatal "Deployed URL is required"; fi
+if [[ -z ${GITHUB_TOKEN} ]]; then fatal "GitHub Token is required"; fi
+if [[ -z ${TRAVIS_REPO_SLUG} ]]; then fatal "Travis Repo slug (user/repo) is required"; fi
+if [[ -z ${TRAVIS_PULL_REQUEST} ]]; then fatal "Travis pull request is required"; fi
 
-if [[ "${TRAVIS_REPO_SLUG}" != "" && "${TRAVIS_PULL_REQUEST}" != "" && "${GITHUB_TOKEN}" != "" ]];
+info "Checking whether ${TRAVIS_REPO_SLUG} #${TRAVIS_PULL_REQUEST} mentions the deployed URL on GitHub..."
+if [[ "${DEPLOYED_URL}" != "" ]];
 then
-    info "Checking whether ${TRAVIS_REPO_SLUG} #${TRAVIS_PULL_REQUEST} mentions the deployed URL on GitHub..."
-    if [[ "${DEPLOYED_URL}" != "" ]];
+    # Only make a comment mentioning the deploy if no other comment has posted the URL yet.
+    STAGING_LINK=$(curl -s -X GET https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments | grep ${DEPLOYED_URL})
+    if [[ ${STAGING_LINK} == "" ]];
     then
-        # Only make a comment mentioning the deploy if no other comment has posted the URL yet.
-        STAGING_LINK=$(curl -s -X GET https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments | grep ${DEPLOYED_URL})
-        if [[ ${STAGING_LINK} == "" ]];
-        then
-            info "Commenting URL to GitHub..."
-            curl -H "Authorization: token ${GITHUB_TOKEN}" \
-                 -X POST \
-                 -d "{\"body\": \"Staging instance deployed by Travis CI!\n Running at ${DEPLOYED_URL}\"}" \
-                 https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
-        else
-            info "Found existing comment mentioning link:\n${STAGING_LINK}"
-        fi
+        info "Commenting URL to GitHub..."
+        curl -H "Authorization: token ${GITHUB_TOKEN}" \
+              -X POST \
+              -d "{\"body\": \"Staging instance deployed by Travis CI!\n Running at ${DEPLOYED_URL}\"}" \
+              https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+    else
+        info "Found existing comment mentioning link:\n${STAGING_LINK}"
     fi
 fi
