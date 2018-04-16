a0c224e82e0e1e4af11fa8c8796edb870750d8a7
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-04-16 09:41:47 -0400

    Re-order things

--- util/deploy-comment.sh
+++ util/deploy-comment.sh
@@ -7,18 +7,17 @@ set -e
 set -o pipefail
 
 DEPLOYED_URL="$1"
-debug "DEPLOYED_URL is ${DEPLOYED_URL}"
 
 REPO_DIR="$(dirname "$0")/.."
 source "${REPO_DIR}/util/logging.sh"
 
-if [[ -z "${GITHUB_TOKEN}" ]];
-then fatal "GitHub Token is required";
-else debug "DEBUG:GitHub token detected.";
-fi
 if [[ -z "${DEPLOYED_URL}" ]];
 then fatal "Deployed URL is required";
 else debug "Deployed URL: ${DEPLOYED_URL}";
+fi
+if [[ -z "${GITHUB_TOKEN}" ]];
+then fatal "GitHub Token is required";
+else debug "GitHub token detected.";
 fi
 if [[ -z "${TRAVIS_REPO_SLUG}" ]];
 then fatal "Travis Repo slug (user/repo) is required";
